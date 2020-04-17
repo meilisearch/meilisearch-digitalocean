dfbf2f185f8bf3908ca11ce48f9ca8707c34e091
Samuel Jimenez <sjimenezre@gmail.com>
2020-04-17 14:51:49 +0200

    Test script execution

--- scripts/per-instance/001.first-launch.sh
+++ scripts/per-instance/001.first-launch.sh
@@ -1,4 +1,4 @@
 # This script will be installed in /var/lib/cloud/scripts/per-instance
 # and will be run automatically by DO at the droplet creation
 
-echo "First install running"
+mkdir /root/some_dir_to_test
