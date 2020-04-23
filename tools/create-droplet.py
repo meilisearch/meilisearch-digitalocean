8a1e796cacff0471593662bfdb9f976042f465a2
Samuel Jimenez <sjimenezre@gmail.com>
2020-04-23 17:11:30 +0200

    First script: set environment

--- tools/create-droplet.py
+++ tools/create-droplet.py
@@ -2,18 +2,31 @@ import digitalocean
 import os
 import requests
 
+# Script settings
+
 DIGITALOCEAN_ACCESS_TOKEN=os.getenv("DIGITALOCEAN_ACCESS_TOKEN")
 DIGITALOCEAN_END_POINT="https://api.digitalocean.com/v2"
+
+# Starting snapshot for Droplet
+
 MEILI_VERSION="" # 0.10.0
 MEILI_IMG_SLUG="TestSam" # MeiliSearch
+
+# Droplet settings
 
 DROPLET_NAME="TestSam-AUTO"
 DROPLET_TAGS=["SAM", "TEST"]
 SSH_KEYS_FINGERPRINTS=[
     "d4:b1:a5:ce:10:01:27:14:44:aa:a9:8e:41:bd:39:bc"
 ]
-USER_DATA="This is some data dynamically entered"
 ENABLE_BACKUPS=False
+
+# Meili stating config
+
+USE_API_KEY="true" # String ["true" / "false"]
+MEILISEARCH_API_KEY="123456" # String [Any]
+USE_SSL="true" # String ["true" / "false"]
+DOMAIN_NAME="sam.meilisearch.com" # String [Any]
 
 
 manager = digitalocean.Manager(token=DIGITALOCEAN_ACCESS_TOKEN)
@@ -25,6 +38,9 @@ for img in images:
         meili_img = img
         break
 
+if meili_img is None:
+    raise Exception("Couldn't find the specified image: {} {}".format(MEILI_IMG_SLUG, MEILI_VERSION))
+
 print("{name}: {id}. Regions: {reg}. Tags: {tags}. Size: {size}".format(
                 name=meili_img.name,
                 id=meili_img.id,
@@ -32,6 +48,13 @@ print("{name}: {id}. Regions: {reg}. Tags: {tags}. Size: {size}".format(
                 tags=meili_img.tags,
                 size=meili_img.size_gigabytes,
             ))
+
+# Set initial MeiliSearch config
+USER_DATA="""export USE_API_KEY={USE_API_KEY}
+export MEILISEARCH_API_KEY={MEILISEARCH_API_KEY}
+export USE_SSL={USE_SSL}
+export DOMAIN_NAME={DOMAIN_NAME}
+""".format(USE_API_KEY=USE_API_KEY,MEILISEARCH_API_KEY=MEILISEARCH_API_KEY,USE_SSL=USE_SSL,DOMAIN_NAME=DOMAIN_NAME)
 
 droplet = digitalocean.Droplet(token=os.getenv("DIGITALOCEAN_ACCESS_TOKEN"),
                                name=DROPLET_NAME,
