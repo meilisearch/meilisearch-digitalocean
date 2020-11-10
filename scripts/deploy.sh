488a876d35d425988589d17b3e208da143d7b1bb
Samuel Jimenez <sjimenezre@gmail.com>
2020-11-10 11:49:17 +0100

    Add MEILI_SERVER_PROVIDER

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,6 +19,7 @@ After=systemd-user-sessions.service
 [Service]
 Type=simple
 ExecStart=/usr/bin/meilisearch --db-path /var/lib/meilisearch/data.ms
+Environment="MEILI_SERVER_PROVIDER=digital_ocean"
 
 [Install]
 WantedBy=default.target
