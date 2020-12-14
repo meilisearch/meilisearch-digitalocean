d20a6e8eef6278f4841fe90e16c5295bbc71e8b1
Samuel Jimenez <sjimenezre@gmail.com>
2020-12-14 16:41:03 +0100

    Remove /etc/os-release modification

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -70,27 +70,11 @@ echo "sh /var/opt/meilisearch/scripts/first-login/000-set-meili-env.sh" >> /root
 # Clean up image using DigitalOcean scripts
 curl https://raw.githubusercontent.com/digitalocean/marketplace-partners/master/scripts/cleanup.sh | bash
 
-# Reset os-release file
-cat << EOF > /etc/os-release
-PRETTY_NAME="Debian GNU/Linux 10 (buster)"
-NAME="Debian GNU/Linux"
-VERSION_ID="10"
-VERSION="10 (buster)"
-VERSION_CODENAME=buster
-ID=debian
-HOME_URL="https://www.debian.org/"
-SUPPORT_URL="https://www.debian.org/support"
-BUG_REPORT_URL="https://bugs.debian.org/"
-EOF
-
 # Setup firewalls and Nginx
 ufw allow 'Nginx Full'
 ufw allow 'OpenSSH'
 ufw --force enable
 
-# Delete remaining logs
-rm -rf /root/.ssh/authorized_keys
-rm -rf /tmp/meili-tmp
-rm -rf /var/log/*.log
+
 
 curl https://raw.githubusercontent.com/digitalocean/marketplace-partners/master/scripts/img_check.sh | bash
