161945f9cdd51c1df4919d2423293ea6625360a4
Samuel Jimenez <sjimenezre@gmail.com>
2021-01-06 18:57:50 +0100

    Install MeiliSearch from cloud-init

--- server/tools/build-image.py
+++ server/tools/build-image.py
@@ -12,7 +12,23 @@ DIGITALOCEAN_END_POINT="https://api.digitalocean.com/v2"
 MEILI_VERSION_TAG="v0.17.0"
 SNAPSHOT_NAME="MeiliSearch-{}-Debian-10.3".format(MEILI_VERSION_TAG)
 SIZE_SLUG="s-1vcpu-1gb" # https://developers.digitalocean.com/documentation/changelog/api-v2/new-size-slugs-for-droplet-plan-changes/
-USER_DATA = "#cloud-config\n\npackage_update: true\npackages:\n  - git\n  - curl\n  - ufw\n  - gcc\n  - make\n - nginx\n  - certbot\n  - python-certbot-nginx\n  - qemu-utils\n  - gcc-10"
+USER_DATA = """
+#cloud-config
+
+packages:  
+  - git
+  - curl
+  - ufw
+  - gcc
+  - make
+  - nginx
+  - certbot
+  - python-certbot-nginx
+
+runcmd:  
+  - wget --directory-prefix=/etc/meilisearch/ https://github.com/meilisearch/MeiliSearch/releases/download/v0.17.0/meilisearch.deb
+  - apt install /etc/meilisearch/meilisearch.deb
+"""
 
 # Droplet settings
 
@@ -48,50 +64,50 @@ print("Droplet created")
 wait_for_ssh_availability(droplet)
 print("SSH Port is available")
 
-# Execute deploy script via SSH
+# # Execute deploy script via SSH
 
-commands = [
-    "apt update",
-    "apt update", # Needed ?
-    "apt install curl -y",
-    "curl https://raw.githubusercontent.com/meilisearch/meilisearch-digital-ocean/{}/scripts/deploy.sh | sh".format(MEILI_VERSION_TAG),
-]
+# commands = [
+#     "apt update",
+#     "apt update", # Needed ?
+#     "apt install curl -y",
+#     "curl https://github.com/meilisearch/meilisearch-digitalocean/blob/use_agnostic_scripts/scripts/deploy.sh | sh".format(MEILI_VERSION_TAG),
+# ]
 
-for cmd in commands:
-    ssh_command = "ssh {user}@{host} -o StrictHostKeyChecking=no '{cmd}'".format(
-        user='root',
-        host=droplet.ip_address,
-        cmd=cmd,
-    )
-    print("EXECUTE COMMAND:", ssh_command)
-    os.system(ssh_command)
-    time.sleep(5)
+# for cmd in commands:
+#     ssh_command = "ssh {user}@{host} -o StrictHostKeyChecking=no '{cmd}'".format(
+#         user='root',
+#         host=droplet.ip_address,
+#         cmd=cmd,
+#     )
+#     print("EXECUTE COMMAND:", ssh_command)
+#     os.system(ssh_command)
+#     time.sleep(5)
 
 
-# TODO: Add a check by HTTP request to IP. If fail no build.
+# # TODO: Add a check by HTTP request to IP. If fail no build.
 
-# Power down droplet
+# # Power down droplet
 
-print("Powering down droplet")
+# print("Powering down droplet")
 
-shutdown = droplet.shutdown(return_dict=True)
+# shutdown = droplet.shutdown(return_dict=True)
 
-while True:
-    d = droplet.get_actions()
-    if d[0].type == "shutdown" and d[0].status == "completed":
-        print("Droplet is OFF")
-        break
+# while True:
+#     d = droplet.get_actions()
+#     if d[0].type == "shutdown" and d[0].status == "completed":
+#         print("Droplet is OFF")
+#         break
 
-print("Creating a snapshot: {}".format(SNAPSHOT_NAME))
+# print("Creating a snapshot: {}".format(SNAPSHOT_NAME))
 
-take_snapshot = droplet.take_snapshot(SNAPSHOT_NAME, return_dict=True, power_off=False)
+# take_snapshot = droplet.take_snapshot(SNAPSHOT_NAME, return_dict=True, power_off=False)
 
-while True:
-    d = droplet.get_actions()
-    if d[0].type == "snapshot" and d[0].status == "completed":
-        print("Snapshot created: {}".format(SNAPSHOT_NAME))
-        break
+# while True:
+#     d = droplet.get_actions()
+#     if d[0].type == "snapshot" and d[0].status == "completed":
+#         print("Snapshot created: {}".format(SNAPSHOT_NAME))
+#         break
 
-print("Destroying droplet")
-droplet.destroy()
-print("Droplet destroyed")
+# print("Destroying droplet")
+# droplet.destroy()
+# print("Droplet destroyed")
