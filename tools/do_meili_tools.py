4f54e3ebe353b66800419d98933c1f47eb3e4a2f
nicolasvienot <nvienot@gmail.com>
2021-03-23 20:52:45 +0100

    Add pylintrc and first rework of python files

--- tools/do_meili_tools.py
+++ tools/do_meili_tools.py
@@ -2,58 +2,63 @@ import time
 import socket
 import requests
 
+
 def wait_for_droplet_creation(droplet):
     while True:
-        d = droplet.get_actions()
-        if d[0].type == "create" and d[0].status == "completed":
+        actions = droplet.get_actions()
+        if actions[0].type == "create" and actions[0].status == "completed":
             break
         time.sleep(2)
+
 
 def wait_for_ssh_availability(droplet):
     while True:
         time.sleep(2)
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
-            s.connect((droplet.ip_address, 22))
-            s.shutdown(2)
-            s.close()
+            sock.connect((droplet.ip_address, 22))
+            sock.shutdown(2)
+            sock.close()
             return
         except Exception:
             continue
+
 
 def wait_for_health_check(droplet):
     while True:
         time.sleep(2)
         try:
             resp = requests.get("http://{}/health".format(droplet.ip_address))
-            if resp.status_code >=200 and resp.status_code < 300:
+            if resp.status_code >= 200 and resp.status_code < 300:
                 return
-        except Exception as e:
-                continue
+        except Exception:
+            continue
+
 
 def wait_for_droplet_shutdown(droplet):
     while True:
         try:
-            d = droplet.get_actions()
-            for act in d:
-                if act.type == "shutdown" and d[0].status == "completed":
+            actions = droplet.get_actions()
+            for act in actions:
+                if act.type == "shutdown" and actions[0].status == "completed":
                     return
-        except Exception as e:
-            print("   Exception: {}".format(e))
+        except Exception as err:
+            print("   Exception: {}".format(err))
             return
+
 
 def wait_for_snapshot_creation(droplet):
     while True:
         time.sleep(2)
-        d = droplet.get_actions()
-        if d[0].type == "snapshot" and d[0].status == "completed":
+        actions = droplet.get_actions()
+        if actions[0].type == "snapshot" and actions[0].status == "completed":
             return
         try:
             time.sleep(2)
-            d = droplet.get_actions()
-            if d[0].type == "snapshot" and d[0].status == "completed":
+            actions = droplet.get_actions()
+            if actions[0].type == "snapshot" and actions[0].status == "completed":
                 return
-        except Exception as e:
-            print("   Exception: {}".format(e))
+        except Exception as err:
+            print("   Exception: {}".format(err))
             time.sleep(300)
             return
