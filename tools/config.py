db3eed8356613e58917f2bec8ea19bd24b938cc9
Nicolas Viénot <nvienot@student.42.fr>
2021-03-29 21:37:14 +0200

    Remove ssh commands and add cloud-config in USER_DATA (#152)
    * Remove ssh commands and add cloud-config in USER_DATA
    * Remove commands in build_image.py
    * Use could-config.yaml from cloud-scripts

--- tools/config.py
+++ tools/config.py
@@ -5,12 +5,6 @@ import requests
 
 MEILI_CLOUD_SCRIPTS_VERSION_TAG = 'v0.19.0'
 
-# Update with the fingerprint of your own SSH key after uploading it to DigitalOcean Settings => Security
-
-SSH_KEYS_FINGERPRINTS = [
-    'd4:b1:a5:ce:10:01:27:14:44:aa:a9:8e:41:bd:39:bc'
-]
-
 # Script settings
 
 DIGITALOCEAN_ACCESS_TOKEN = os.getenv('DIGITALOCEAN_ACCESS_TOKEN')
@@ -19,10 +13,10 @@ SNAPSHOT_NAME = 'MeiliSearch-{}-Debian-10.3'.format(
     MEILI_CLOUD_SCRIPTS_VERSION_TAG)
 # https://developers.digitalocean.com/documentation/changelog/api-v2/new-size-slugs-for-droplet-plan-changes/
 SIZE_SLUG = 's-1vcpu-1gb'
-PROVIDER_NAME = 'digitalocean'
 USER_DATA = requests.get(
-    'https://raw.githubusercontent.com/meilisearch/cloud-scripts/{}/scripts/cloud-config.yaml'.format(MEILI_CLOUD_SCRIPTS_VERSION_TAG)).text.replace('unknown_provider', PROVIDER_NAME)
-
+    'https://raw.githubusercontent.com/meilisearch/cloud-scripts/{}/scripts/providers/digitalocean/cloud-config.yaml'
+    .format(MEILI_CLOUD_SCRIPTS_VERSION_TAG)
+).text
 
 # Droplet settings
 
