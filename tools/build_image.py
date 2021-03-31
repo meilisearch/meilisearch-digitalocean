fddcaf052bb0f03ac2c4cf888438e671dfdbafcb
nicolasvienot <nvienot@gmail.com>
2021-03-31 15:08:16 +0200

    CI-TEST try except on build_python

--- tools/build_image.py
+++ tools/build_image.py
@@ -42,44 +42,58 @@ droplet.create()
 
 # Wait for Droplet to be created
 
-
-state = 'in-progress'
-actions = droplet.get_actions()
-while state != 'completed':
-    for action in actions:
-        time.sleep(2)
-        if action.type == 'create':
-            action.load()
-            print("Droplet type {} | Droplet status {}". format(
-                action.type, action.status))
-            state = action.status
+try:
+    state = 'in-progress'
+    actions = droplet.get_actions()
+    while state != 'completed':
+        for action in actions:
+            time.sleep(2)
+            if action.type == 'create':
+                action.load()
+                print("Droplet type {} | Droplet status {}". format(
+                    action.type, action.status))
+                state = action.status
+except Exception as err:
+    print("   Exception at action.type == 'create': {}".format(err))
+    raise
 
 # wait_for_droplet_creation(droplet)
 # droplet = droplet.load()
 
-while not droplet.ip_address:
-    print('droplet has no ip assigned yet')
-    time.sleep(2)
-    droplet.load()
+try:
+    while not droplet.ip_address:
+        print('droplet has no ip assigned yet')
+        time.sleep(2)
+        droplet.load()
+except Exception as err:
+    print("   Exception at droplet.ip_address: {}".format(err))
+    raise
 
 print('   Droplet created. IP: {}, ID: {}'.format(
     droplet.ip_address, droplet.id))
 
 # Wait for Health check after configuration is finished
-
-while not droplet.status == 'active':
-    droplet.get_actions()
-    droplet.load()
-    print('Current status... ' + droplet.status)
-    time.sleep(2)
+try:
+    while not droplet.status == 'active':
+        droplet.get_actions()
+        droplet.load()
+        print('Current status... ' + droplet.status)
+        time.sleep(2)
+except Exception as err:
+    print("   Exception at droplet.status == 'active': {}".format(err))
+    raise
 
 print('Waiting for Health check (may take a few minutes: config and reboot)')
-HEALTH = wait_for_health_check(droplet, timeout_seconds=600)
-if HEALTH == STATUS_OK:
-    print('   Instance is healthy')
-else:
-    print('   Timeout waiting for health check')
-    destroy_droplet_and_exit(droplet)
+try:
+    HEALTH = wait_for_health_check(droplet, timeout_seconds=600)
+    if HEALTH == STATUS_OK:
+        print('   Instance is healthy')
+    else:
+        print('   Timeout waiting for health check')
+        destroy_droplet_and_exit(droplet)
+except Exception as err:
+    print("   Exception at wait_for_health_check: {}".format(err))
+    raise
 
 # Check version
 
@@ -88,16 +102,22 @@ try:
     check_meilisearch_version(
         droplet, conf.MEILI_CLOUD_SCRIPTS_VERSION_TAG)
 except Exception as err:
-    print("   Exception: {}".format(err))
-    destroy_droplet_and_exit(droplet)
+    print("   Exception at check_meilisearch_version: {}".format(err))
+    raise
 print('   Version of meilisearch match!')
 
 # Power down Droplet
-
-print('Powering down droplet...')
-shutdown = droplet.shutdown(return_dict=False)
-shutdown.wait()
+try:
+    print('Powering down droplet...')
+    shutdown = droplet.shutdown(return_dict=False)
+    print('trigger shutdown droplet...')
+    shutdown.wait()
+except Exception as err:
+    print("  Exception  shutdown.wait(): {}".format(err))
+    raise
 print('   Droplet is OFF')
+
+time.sleep(15)
 
 # Create snapshot from Droplet
 
@@ -105,15 +125,23 @@ if len(sys.argv) > 1:
     SNAPSHOT_NAME = sys.argv[1]
 else:
     SNAPSHOT_NAME = conf.SNAPSHOT_NAME
-
-print('Creating a snapshot: {}'.format(SNAPSHOT_NAME))
-take_snapshot = droplet.take_snapshot(
-    SNAPSHOT_NAME, return_dict=False, power_off=True)
-take_snapshot.wait()
+try:
+    print('Creating a snapshot: {}'.format(SNAPSHOT_NAME))
+    take_snapshot = droplet.take_snapshot(
+        SNAPSHOT_NAME, return_dict=False, power_off=True)
+    print('trigger take_snapshot...')
+    take_snapshot.wait()
+except Exception as err:
+    print("  Exception at take_snapshot: {}".format(err))
+    raise
 print('   Snapshot created: {}'.format(SNAPSHOT_NAME))
 
 # Desroy Droplet
 
 print('Destroying Droplet...')
-droplet.destroy()
+try:
+    droplet.destroy()
+except Exception as err:
+    print("  Exception at destroy: {}".format(err))
+    raise
 print('   Droplet destroyed')
