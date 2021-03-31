338a715b1872eaafe7f841db377cb4fa94534723
nicolasvienot <nvienot@gmail.com>
2021-03-31 14:39:24 +0200

    CI-TEST try to use new wait() in build_image.py

--- tools/build_image.py
+++ tools/build_image.py
@@ -1,11 +1,19 @@
-import sys
 import time
-import requests
+import sys
 import digitalocean
+import requests
+from requests.packages.urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
 from utils import wait_for_droplet_creation, wait_for_health_check, \
     wait_for_droplet_shutdown, wait_for_snapshot_creation, \
     destroy_droplet_and_exit, check_meilisearch_version, STATUS_OK
 import config as conf
+
+# Configure requests on digitalocean
+manager = digitalocean.Manager(token=conf.DIGITALOCEAN_ACCESS_TOKEN)
+retry = Retry(connect=3)
+adapter = HTTPAdapter(max_retries=retry)
+manager._session.mount('https://', adapter)
 
 # Create droplet
 
@@ -22,14 +30,48 @@ droplet = digitalocean.Droplet(token=conf.DIGITALOCEAN_ACCESS_TOKEN,
 droplet.create()
 
 
+# def wait_own(event, dropl, status="completed"):
+#     state = 'in-progress'
+#     actions = dropl.get_actions()
+#     while state != 'completed':
+#         for action in actions:
+#             time.sleep(2)
+#             action.load()
+#             if action.id == event['action']['id']:
+#                 state = action.status
+
 # Wait for Droplet to be created
 
-wait_for_droplet_creation(droplet)
-droplet = droplet.load()
+
+state = 'in-progress'
+actions = droplet.get_actions()
+while state != 'completed':
+    for action in actions:
+        time.sleep(2)
+        if action.type == 'create':
+            action.load()
+            print("Droplet type {} | Droplet status {}". format(
+                action.type, action.status))
+            state = action.status
+
+# wait_for_droplet_creation(droplet)
+# droplet = droplet.load()
+
+while not droplet.ip_address:
+    print('droplet has no ip assigned yet')
+    time.sleep(2)
+    droplet.load()
+
 print('   Droplet created. IP: {}, ID: {}'.format(
     droplet.ip_address, droplet.id))
 
 # Wait for Health check after configuration is finished
+
+while not droplet.status == 'active':
+    droplet.get_actions()
+    droplet.load()
+    print('Current status... ' + droplet.status)
+    time.sleep(2)
 
 print('Waiting for Health check (may take a few minutes: config and reboot)')
 HEALTH = wait_for_health_check(droplet, timeout_seconds=600)
@@ -53,25 +95,8 @@ print('   Version of meilisearch match!')
 # Power down Droplet
 
 print('Powering down droplet...')
-droplet.shutdown()
-time.sleep(30)
-droplet.power_off()
-time.sleep(10)
-
-# AUTH = "Bearer {}".format(conf.DIGITALOCEAN_ACCESS_TOKEN)
-# headers = {
-#     'Content-Type': 'application/json',
-#     'Authorization': AUTH
-# }
-# URL = 'https://api.digitalocean.com/v2/droplets/{}/actions'.format(droplet.id)
-# payload = {"type": "shutdown"}
-# r = requests.post(URL, json=payload, headers=headers, timeout=10)
-
-# try:
-#     wait_for_droplet_shutdown(droplet)
-# except Exception as err:
-#     print("   Exception: {}".format(err))
-#     destroy_droplet_and_exit(droplet)
+shutdown = droplet.shutdown(return_dict=False)
+shutdown.wait()
 print('   Droplet is OFF')
 
 # Create snapshot from Droplet
@@ -83,8 +108,8 @@ else:
 
 print('Creating a snapshot: {}'.format(SNAPSHOT_NAME))
 take_snapshot = droplet.take_snapshot(
-    SNAPSHOT_NAME, return_dict=True, power_off=False)
-wait_for_snapshot_creation(droplet)
+    SNAPSHOT_NAME, return_dict=False, power_off=True)
+take_snapshot.wait()
 print('   Snapshot created: {}'.format(SNAPSHOT_NAME))
 
 # Desroy Droplet
