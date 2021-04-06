a44c0bd4c87b3e057f940ea537f599e5472e7e74
Nicolas Viénot <nvienot@student.42.fr>
2021-04-06 13:00:23 +0200

    Create a script to test the image built and add the full process to CI (#157)
    * Add DROPLET_TEST_NAME
    
    * Update healthcheck with timeout and add versioncheck
    
    * Add new healthcheck and versioncheck in build_image
    
    * Remove create_droplet.py
    
    * Add test_image.py
    
    * Add destroy_image.py
    
    * clean and comment
    
    * Update versioncheck
    
    * Output exception for versioncheck error
    
    * Add github action
    
    * Increase sleep on healthcheck
    
    * Add timeout and verify on requests
    
    * Add exception on powering droplet down
    
    * Remove the return on droplet shutdown
    
    * Update sleeps and replace shutdown by power_off
    
    * Revert to shutdown and add sleep before shutdown
    
    * Replace shutdown from python-do by request to do api
    
    * Add sleep at start of wait_for_droplet_shutdown
    
    * Adding timeout for requests in python digitalocean
    
    * Add timeout to own request for shutdown
    
    * not using own request and wait_for_droplet_shutdown anymore
    
    * CI-TEST try to use new wait() in build_image.py
    
    * CI-TESTpython version on CI
    
    * CI-TEST Python version CI to 3.8
    
    * CI-TEST Fix python version CI
    
    * CI-TEST try except on build_python
    
    * TEST-CI test
    
    * Reading documentation is a good thing
    
    * CI-TEST Test ubuntu 18.04
    
    * Clean utils and functions checking actions on droplet from do api
    
    * Revert to clean build and test with error check after CI-TESTS
    
    * CI-TEST Pylint 100 and last-test on ubuntu 18
    
    * Rework wait_for_xxx and try to stop on ReadTimeout error
    
    * Clean and change sleeps in utils.py
    
    * CI on macos-latest
    
    * Revert
    
    * Github Actions was down, revert to final version!

--- tools/test_image.py
+++ tools/test_image.py
@@ -1,23 +1,49 @@
 import sys
 import digitalocean
 from utils import wait_for_droplet_creation, wait_for_health_check, \
-    wait_for_droplet_shutdown, wait_for_snapshot_creation, \
-    destroy_droplet_and_exit, check_meilisearch_version, STATUS_OK
+    wait_for_droplet_shutdown, check_meilisearch_version, destroy_droplet_and_exit, STATUS_OK
+
 import config as conf
 
-# Create droplet
+if len(sys.argv) > 1:
+    SNAPSHOT_NAME = sys.argv[1]
+else:
+    raise Exception('No snapshot name specified')
 
-print('Creating droplet...')
+print('Running test for image named: {name}...'.format(
+    name=SNAPSHOT_NAME))
+
+# Get the snapshot for the test
+
+manager = digitalocean.Manager(token=conf.DIGITALOCEAN_ACCESS_TOKEN)
+images = manager.get_images()
+
+MEILI_IMG = None
+for img in images:
+    if img.name == SNAPSHOT_NAME:
+        MEILI_IMG = img
+        print('Found image: {name} created at {created_at}'.format(
+            name=img.name,
+            created_at=img.created_at
+        ))
+        break
+
+if MEILI_IMG is None:
+    raise Exception('Couldn\'t find the specified image: {}'.format(
+        SNAPSHOT_NAME))
+
+# Create droplet from the retreived snapshot
 
 droplet = digitalocean.Droplet(token=conf.DIGITALOCEAN_ACCESS_TOKEN,
-                               name=conf.DROPLET_NAME,
+                               name=conf.DROPLET_TEST_NAME,
                                region='lon1',  # London
-                               image='debian-10-x64',  # Debian 10.3
+                               image=MEILI_IMG.id,
                                size_slug=conf.SIZE_SLUG,
-                               tags=['marketplace'],
-                               backups=conf.ENABLE_BACKUPS,
-                               user_data=conf.USER_DATA)
+                               tags=['meisearch-digitalocean-ci'],
+                               backups=conf.ENABLE_BACKUPS)
 droplet.create()
+
+print('Creating droplet...')
 
 # Wait for Droplet to be created
 
@@ -33,7 +59,7 @@ print('   Droplet created. IP: {}, ID: {}'.format(
 
 # Wait for Health check after configuration is finished
 
-print('Waiting for Health check (may take a few minutes: config and reboot)')
+print('Waiting for Health check (may take a few minutes)')
 HEALTH = wait_for_health_check(droplet, timeout_seconds=600)
 if HEALTH == STATUS_OK:
     print('   Instance is healthy')
@@ -64,25 +90,6 @@ except Exception as err:
     destroy_droplet_and_exit(droplet)
 
 print('   Droplet is OFF')
-
-# Create snapshot from Droplet
-
-if len(sys.argv) > 1:
-    SNAPSHOT_NAME = sys.argv[1]
-else:
-    SNAPSHOT_NAME = conf.SNAPSHOT_NAME
-
-print('Creating a snapshot: {}'.format(SNAPSHOT_NAME))
-
-try:
-    take_snapshot = droplet.take_snapshot(
-        SNAPSHOT_NAME, return_dict=False, power_off=True)
-    wait_for_snapshot_creation(droplet)
-except Exception as err:
-    print('  Exception: {}'.format(err))
-    destroy_droplet_and_exit(droplet)
-
-print('   Snapshot created: {}'.format(SNAPSHOT_NAME))
 
 # Destroy Droplet
 
--- tools/utils.py
+++ tools/utils.py
@@ -1,19 +1,32 @@
+import datetime
+import sys
 import time
 import socket
 import requests
 
+STATUS_OK = 0
+STATUS_TIMEOUT = 1
+
 
 def wait_for_droplet_creation(droplet):
-    while True:
+    try:
         actions = droplet.get_actions()
-        if actions[0].type == "create" and actions[0].status == "completed":
-            break
-        time.sleep(2)
+        while True:
+            for act in actions:
+                if act.type == 'create':
+                    act.load()
+                    print('   Action {} is {}'.format(
+                        act.type, act.status))
+                    if act.status == 'completed':
+                        return
+            time.sleep(4)
+    except Exception as err:
+        print('   Exception: {}'.format(err))
+        raise
 
 
 def wait_for_ssh_availability(droplet):
     while True:
-        time.sleep(2)
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             sock.connect((droplet.ip_address, 22))
@@ -22,43 +35,79 @@ def wait_for_ssh_availability(droplet):
             return
         except Exception:
             continue
-
-
-def wait_for_health_check(droplet):
-    while True:
         time.sleep(2)
+
+
+def wait_for_health_check(droplet, timeout_seconds=None):
+    start_time = datetime.datetime.now()
+    while timeout_seconds is None \
+            or check_timeout(start_time, timeout_seconds) is not STATUS_TIMEOUT:
         try:
-            resp = requests.get("http://{}/health".format(droplet.ip_address))
+            resp = requests.get(
+                'http://{}/health'.format(droplet.ip_address), verify=False, timeout=10)
             if resp.status_code >= 200 and resp.status_code < 300:
-                return
+                return STATUS_OK
         except Exception:
-            continue
+            pass
+        time.sleep(2)
+    return STATUS_TIMEOUT
 
 
 def wait_for_droplet_shutdown(droplet):
-    while True:
-        try:
-            actions = droplet.get_actions()
+    try:
+        actions = droplet.get_actions()
+        while True:
             for act in actions:
-                if act.type == "shutdown" and actions[0].status == "completed":
-                    return
-        except Exception as err:
-            print("   Exception: {}".format(err))
-            return
+                if act.type == 'shutdown':
+                    act.load()
+                    print('   Action {} is {}'.format(
+                        act.type, act.status))
+                    if act.status == 'completed':
+                        return
+            time.sleep(4)
+    except Exception as err:
+        print('   Exception: {}'.format(err))
+        raise
 
 
 def wait_for_snapshot_creation(droplet):
-    while True:
-        time.sleep(2)
+    try:
         actions = droplet.get_actions()
-        if actions[0].type == "snapshot" and actions[0].status == "completed":
-            return
-        try:
-            time.sleep(2)
-            actions = droplet.get_actions()
-            if actions[0].type == "snapshot" and actions[0].status == "completed":
-                return
-        except Exception as err:
-            print("   Exception: {}".format(err))
-            time.sleep(300)
-            return
+        while True:
+            for act in actions:
+                if act.type == 'snapshot':
+                    act.load()
+                    print('   Action {} is {}'.format(
+                        act.type, act.status))
+                    if act.status == 'completed':
+                        return
+            time.sleep(4)
+    except Exception as err:
+        print('   Exception: {}'.format(err))
+        time.sleep(300)
+        raise
+
+
+def check_meilisearch_version(droplet, version):
+    resp = requests.get(
+        'http://{}/version'.format(droplet.ip_address), verify=False, timeout=10).json()
+    if resp['pkgVersion'] in version:
+        return
+    raise Exception(
+        '    The version of meilisearch ({}) does not match the droplet ({})'.format(version, resp['pkgVersion']))
+
+
+def destroy_droplet_and_exit(droplet):
+    print('   Destroying droplet {}'.format(droplet.id))
+    droplet.destroy()
+    print('ENDING PROCESS WITH EXIT CODE 1')
+    sys.exit(1)
+
+# GENERAL
+
+
+def check_timeout(start_time, timeout_seconds):
+    elapsed_time = datetime.datetime.now() - start_time
+    if elapsed_time.total_seconds() > timeout_seconds:
+        return STATUS_TIMEOUT
+    return STATUS_OK
