c85456529d9841982a84d83cd654f55c0889e6c3
alallema <amelie@meilisearch.com>
2021-10-04 12:15:06 +0200

    Test of cloud script with new dump folder

--- tools/config.py
+++ tools/config.py
@@ -9,13 +9,12 @@ MEILI_CLOUD_SCRIPTS_VERSION_TAG = 'v0.22.0'
 
 DIGITALOCEAN_ACCESS_TOKEN = os.getenv('DIGITALOCEAN_ACCESS_TOKEN')
 DIGITALOCEAN_END_POINT = 'https://api.digitalocean.com/v2'
-SNAPSHOT_NAME = 'MeiliSearch-{}-Debian-10.3'.format(
+SNAPSHOT_NAME = 'MeiliSearch-{}-Debian-10.3-TEST-DUMP'.format(
     MEILI_CLOUD_SCRIPTS_VERSION_TAG)
 # https://developers.digitalocean.com/documentation/changelog/api-v2/new-size-slugs-for-droplet-plan-changes/
 SIZE_SLUG = 's-1vcpu-1gb'
 USER_DATA = requests.get(
-    'https://raw.githubusercontent.com/meilisearch/cloud-scripts/{}/scripts/providers/digitalocean/cloud-config.yaml'
-    .format(MEILI_CLOUD_SCRIPTS_VERSION_TAG)
+    'https://raw.githubusercontent.com/meilisearch/cloud-scripts/dump-dir/scripts/providers/digitalocean/cloud-config.yaml'
 ).text
 
 # Droplet settings
